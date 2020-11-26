3553235ac187986e811576d88e724a59174404a7
Ethan Guthrie <guthrieec@cofc.edu>
2020-11-25 21:37:36 -0500

    Remove versioneer

---
+++ distribute.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# Getting new version.
+currVersion=$( cat VERSION )
+echo "Current version: $currVersion"
+echo "Enter new version name: "
+read newVersion
+echo $newVersion > version.txt
+echo "Version: $currVersion -> $newVersion"
+
+# Adding and commit new version.
+git add VERSION
+git commit -m "Update version number"
+git push origin master
+
+# Tagging newest version and uploading tarball to GitHub.
+git tag $newVersion
+python3 setup.py register sdist upload
+
+# Uploading to PyPi.
+sudo python3 setup.py develop
+sudo python3 setup.py sdist bdist_wheel
+python3 -m twine upload --repository pypi dist/*
--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 """
 setup.py
 ~~~~~~~~
-
+Sets up EzPyZ. Uses setup.cfg for configuration information.
 """
 
 import setuptools
-import versioneer
-
-with open("README.md", "r") as readme_file:
-    LONG_DESCRIPTION = readme_file.read()
 
 # Setup will be done using the information in setup.cfg
-setuptools.setup(
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
-)
+setuptools.setup()
