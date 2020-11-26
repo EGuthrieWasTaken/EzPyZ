ecbad82302b4248f5549b5a446a2ceba27110a51
Ethan Guthrie <guthrieec@cofc.edu>
2020-11-25 19:49:41 -0500

    Fix versioneer

--- setup.py
+++ setup.py
@@ -10,15 +10,8 @@ import versioneer
 with open("README.md", "r") as readme_file:
     LONG_DESCRIPTION = readme_file.read()
 
-# These should all remain the same.
-NAME = "EzPyZ"
-DESCRIPTION = ""
-AUTHOR = "Ethan Guthrie"
-PACKAGES = setuptools.find_packages()
-
-# These should change.
-version = versioneer.get_version()
-cmdclass = versioneer.get_cmdclass()
-
 # Setup will be done using the information in setup.cfg
-setuptools.setup()
+setuptools.setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
+)
